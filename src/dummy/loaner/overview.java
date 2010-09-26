074e8676ab4b5caeffd43fef04bd862e2191be90
dummy3k <l4711@gmx.net>
2010-09-26 18:08:45 +0200

    get contact id, get contact by id

--- src/dummy/loaner/overview.java
+++ src/dummy/loaner/overview.java
@@ -2,6 +2,7 @@ package dummy.loaner;
 
 import android.app.Activity;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -15,21 +16,44 @@ import android.widget.Toast;
 
 public class overview extends Activity {
 	private static final int PICK_CONTACT = 1;
-	private TextView foo;
+	private TextView lblContactUri;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        foo = (TextView)findViewById(R.id.TextView01);
+        lblContactUri = (TextView)findViewById(R.id.TextView01);
     }
 
 	public void myClickHandler(View view) {
-		Intent intent = new Intent(Intent.ACTION_PICK, People.CONTENT_URI);  
+//		Intent.ACTION_GET_CONTENT
+		Intent intent = new Intent(Intent.ACTION_PICK, People.CONTENT_URI);
 		startActivityForResult(intent, PICK_CONTACT);  
+	}
+	
+	public void debugHandler(View view) {
+//		String uri_str = "content://contacts/people/1";
+//		String id = (String)lblContactUri.getText();
+		long id = 1;
+		Uri.Builder builder = new Uri.Builder();
+		builder.scheme("content");
+		builder.appendEncodedPath("/contacts/people/1");
+		Uri contactData = ContentUris.withAppendedId(People.CONTENT_URI, id);
+//		content://contacts/people/1
+		lblContactUri.setText(contactData.toString());
 		
-		//foo.setText("Hello World");
+		Cursor c = managedQuery(contactData, null, null, null, null);
+		if (c == null) {
+			lblContactUri.setText("c isnull");
+			return;
+		}
+
+		if (c.moveToFirst()) {
+			String name = c.getString(c.getColumnIndexOrThrow(People.NAME));
+			lblContactUri.setText(name);
+		}
+
 	}
 	
 	@Override
@@ -40,11 +64,11 @@ public class overview extends Activity {
 			case (PICK_CONTACT):
 				if (resultCode == Activity.RESULT_OK) {
 					Uri contactData = data.getData();
-					Cursor c = managedQuery(contactData, null, null, null, null);
-					if (c.moveToFirst()) {
-						String name = c.getString(c.getColumnIndexOrThrow(People.NAME));
-						foo.setText(name);
-					}
+					//contactData.
+					long id =ContentUris.parseId(contactData);
+					lblContactUri.setText(Long.toString(id));
+//					lblContactUri.setText(contactData.toString());
+//					lblContactUri.setText(contactData.get);
 				}
 				break;
 		}
